12a7cbca1be62106cacbce6eff658e5f8dd1c141
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-24 17:13:01 +0200

    Added create method and form test

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,20 @@
 class AuthorsController < ApplicationController
+  # POST /authors/new
+  def create
+    @author = Author.new(author_params)
+
+    @author.save
+    #redirect_to @author
+    render plain: ''
+  end
+
   # GET /authors/new
   def new
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
 
-  it "should render withour error" do
+  it "should render without error" do
     visit new_author_path
   end
 
@@ -12,5 +12,15 @@ describe "New author page", type: :feature do
     expect(page).to have_field('First name')
     expect(page).to have_field('Last name')
     expect(page).to have_field('Homepage')
+  end
+
+  it "should save the author on submit" do
+    visit new_author_path
+    fill_in "author_first_name", with: "Alan"
+    fill_in "author_last_name", with: "Turing"
+    fill_in "author_homepage", with: "http://wikipedia.org/Alan_Turing"
+    find('input[type="submit"]').click
+
+    #puts "List off all Authors: #{Authors.all}"
   end
 end
