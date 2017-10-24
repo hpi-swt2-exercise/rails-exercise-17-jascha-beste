e0fb69c86004ae9d0279e8941c7784f3fb1f1ca2
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-24 18:42:33 +0200

    Added validation to author last name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "Author", :type => :model do
   let(:author) {Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')}
-  subject{author}
+  let(:invalid_author) {Author.new(first_name: 'Alan', last_name: nil, homepage: 'http://wikipedia.org/Alan_Turing')}
+  
   it "should have correct first name, last name and homepage" do
     expect(author.first_name).to eq "Alan"
     expect(author.last_name).to eq "Turing"
@@ -11,5 +12,9 @@ describe "Author", :type => :model do
 
   it "should have correct name based on first and last name" do
     expect(author.name).to eq "Alan Turing"
+  end
+
+  it "shoudl be invalid if no last name given" do
+    expect(invalid_author).to_not be_valid
   end
 end
