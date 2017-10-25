edcd41396764c0994137e20c4dcb219c7811cd40
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-25 22:42:23 +0200

    added save functionality to authors edit page

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
-describe "Index author page", type: :feature do
+describe "Edit author page", type: :feature do
 
   it "should render without error" do
     visit authors_path
+  end
+
+  it "should save the author edits on submit" do
+
+    alan = FactoryGirl.create :author
+    visit edit_author_path(alan)
+    fill_in "author_last_name", with: "Mathison"
+    find('input[type="submit"]').click
+
+    author = Author.last
+    expect(author.last_name).to eq "Mathison"
   end
 end
