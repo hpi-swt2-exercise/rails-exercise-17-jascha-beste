1c029261608fd4127c651dcd56f82b453bfeb20c
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-25 23:55:32 +0200

    Added text fields for paper venue and year on new paper page

--- spec/features/papers/new_paper_spec.rb
+++ spec/features/papers/new_paper_spec.rb
@@ -5,4 +5,12 @@ describe "New paper page", type: :feature do
   it "should render without error" do
     visit new_paper_path
   end
+
+  it "should have text input for first paper, venue, and year" do
+    visit new_author_path
+
+    expect(page).to have_field('Paper')
+    expect(page).to have_field('Venue')
+    expect(page).to have_field('Year')
+  end
 end
