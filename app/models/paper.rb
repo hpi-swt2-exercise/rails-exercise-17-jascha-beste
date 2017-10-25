f17e27468af7ba31de7b129ffd13fbf38e86afba
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-26 01:05:30 +0200

    Paper now have and belong to authors

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,7 @@
 class Paper < ActiveRecord::Base
+  belongs_to :authors
+  has_many :authors
+  
   validates :title, :venue, presence: true
   validates :year, numericality: true, presence:true
 end
