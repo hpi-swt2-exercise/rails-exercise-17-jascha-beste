12a2f74f8ac8940130bdbb8d8138e0503544d163
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-26 00:56:08 +0200

    Added edit methods for paper

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -12,6 +12,19 @@ class PapersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @paper = Paper.find(params[:id])
+  end
+
+  def update
+    @paper = Paper.find(params[:id])
+
+    if @paper.update(paper_params)
+      redirect_to @paper
+    else
+      render 'edit'
+    end
+  end
 
   def index
     @papers = Paper.all
