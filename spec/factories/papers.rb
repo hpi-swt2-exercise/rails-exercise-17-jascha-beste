81f4889c6f1737e76ace199d24a15a2f01fd1e79
Jascha Beste <jascha.beste@student.hpi.de>
2017-10-26 00:07:08 +0200

    added paper model

---
+++ spec/factories/papers.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :paper do
+    title "MyString"
+venue "MyString"
+year "MyString"
+  end
+
+end
--- spec/features/papers/new_paper_spec.rb
+++ spec/features/papers/new_paper_spec.rb
@@ -6,10 +6,10 @@ describe "New paper page", type: :feature do
     visit new_paper_path
   end
 
-  it "should have text input for first paper, venue, and year" do
-    visit new_author_path
+  it "should have text input for title, venue, and year" do
+    visit new_paper_path
 
-    expect(page).to have_field('Paper')
+    expect(page).to have_field('Title')
     expect(page).to have_field('Venue')
     expect(page).to have_field('Year')
   end
